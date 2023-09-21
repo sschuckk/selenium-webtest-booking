3d3ca6355d74f46162e6c6a041712b99d2179223
Rodrigo Schuck <sschuckk@gmail.com>
2023-09-21 10:06:44 -0400

    Remove pop check and add pytest.ini configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,32 +1,17 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
 
 
 @pytest.fixture(autouse=True, scope="class")
 def setup(request):
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument("--incognito")
-
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    #driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-    #driver = webdriver.Chrome(options=chrome_options)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--incognito")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
     driver.implicitly_wait(5)
     driver.maximize_window()
     driver.get("https://www.booking.com/")
     request.cls.driver = driver
-
-    # try:
-    #     wait = WebDriverWait(driver, 5)
-    #     wait.until(expected_conditions.presence_of_element_located((By.XPATH, "//div[@role='dialog']")))
-    #     driver.find_element(By.CLASS_NAME,
-    #                         "fc63351294.a822bdf511.e3c025e003.fa565176a8.f7db01295e.c334e6f658.ae1678b153").click()
-    # except WebDriverWait:
-    #     print("Pop-up not detected")
-
     yield
     driver.quit()
