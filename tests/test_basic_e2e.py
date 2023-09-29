3491e77cbcd3be1d42904c0093223380e51222cd
Rodrigo Schuck <sschuckk@gmail.com>
2023-09-28 21:50:53 -0400

    Add docstring to test cases

--- tests/test_basic_e2e.py
+++ tests/test_basic_e2e.py
@@ -6,21 +6,23 @@ from utils.base import BaseClass
 class TestSuite(BaseClass):
 
     def test_language(self):
+        """Test language selection choosing US as option."""
         self.base_homepage().get_select_language().click()
         self.base_homepage().get_us_language().click()
 
     def test_currency(self):
-        """Test """
+        """Test currency selection choosing US Dollar as option."""
         self.base_homepage().get_select_currency().click()
         self.base_homepage().get_us_currency().click()
 
     def test_city_destination(self):
+        """Test destination input field by passing a city name."""
         destination = "New York"
         self.base_homepage().get_destination().send_keys(destination)
         self.base_homepage().get_city_destination(destination).click()
 
     def test_dates(self):
-        """Test check-in and check-out dates using the current date + delta"""
+        """Test check-in and check-out dates using the current date + delta date."""
         check_in = datetime.date.today() + datetime.timedelta(days=1)
         check_out = datetime.date.today() + datetime.timedelta(days=3)
         self.base_homepage().get_date_box().click()
@@ -28,7 +30,7 @@ class TestSuite(BaseClass):
         self.base_homepage().get_date_picker(check_out).click()
 
     def test_occupancy(self):
-        """Test occupancy adult with increase and decrease option"""
+        """Test occupancy configuration by setting the number of adult by increasing and decreasing the count."""
         self.base_homepage().get_occupancy_box().click()
         self.base_homepage().get_increase_occupancy_adult().click()
         self.base_homepage().get_increase_occupancy_adult().click()
@@ -36,12 +38,12 @@ class TestSuite(BaseClass):
         self.base_homepage().get_occupancy_button_done().click()
 
     def test_search_options(self):
-        """Test the search button"""
+        """Test the click at search button"""
         self.base_homepage().get_button_search().click()
 
     @pytest.mark.parametrize("star", ["3", "4", "5"])
     def test_filter_rating(self, star):
-        """Test the selection of filters by star ratings with multiple input values"""
+        """Test the selection of filters by star ratings with parameterized multiple input values."""
         self.base_results().filter_by_rating(star).click()
 
     def test_filter_distance(self):
@@ -49,10 +51,11 @@ class TestSuite(BaseClass):
         self.base_results().filter_by_distance(distance="3220").click()
 
     def test_sort_by_price(self):
-        """Test the sort by price dropdown trigger"""
+        """Test the sorting of hotel results by selecting 'sort by price' from a dropdown menu."""
         self.base_results().sort_dropdown_trigger().click()
         self.base_results().sort_by_price().click()
 
     def test_result_list(self):
+        """Verify hotel list results"""
         hotel_lst = self.base_results().get_result_list()
         self.html_pandas(hotel_lst)
